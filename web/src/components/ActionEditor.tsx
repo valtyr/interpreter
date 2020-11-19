443e84d6bb74490de9091b33685b71ef670e1c5b
Valtýr Örn Kjartansson <valtyr@gmail.com>
2020-11-19 10:35:01 +0000

    Continue work on action debugger

--- web/src/components/ActionEditor.tsx
+++ web/src/components/ActionEditor.tsx
@@ -2,32 +2,54 @@ import React, { useRef, useState } from "react";
 
 import ActionSchema from "../schemas/actions.schema.json";
 import MonacoEditor from "react-monaco-editor";
+import Ajv from "ajv";
+import { Button } from "antd";
+import { useTypedDispatch } from "../store";
+
+var ajv = new Ajv();
+var validate = ajv.compile(ActionSchema);
 
 const ActionEditor = () => {
   const monaco = useRef<MonacoEditor | null>(null);
-  const [action, setAction] = useState("");
+  const [action, setAction] = useState(JSON.stringify({ type: "" }, null, 2));
   const [valid, setValid] = useState(false);
 
-  return (
-    <MonacoEditor
-      height="600"
-      language="json"
-      theme="vs-dark"
-      value={action}
-      ref={monaco}
-      onChange={(value) => {
-        setAction(value);
-      }}
-      editorWillMount={(monaco) => {
-        var modelUri = monaco.Uri.parse("a://b/foo.json"); // a made up unique URI for our model
-        var model = monaco.editor.createModel("{}", "json", modelUri);
+  const jsonAction = JSON.parse(action);
 
-        monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
-          validate: true,
-          schemas: [{ uri: "bla", schema: ActionSchema }],
-        });
-      }}
-    />
+  const dispatch = useTypedDispatch();
+
+  return (
+    <>
+      <MonacoEditor
+        key="editor"
+        height="600"
+        language="json"
+        theme="vs-dark"
+        value={action}
+        ref={monaco}
+        onChange={(value) => {
+          setAction(value);
+        }}
+        editorWillMount={(monaco) => {
+          monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+            validate: true,
+            schemas: [
+              {
+                uri: "http://myserver/foo-schema.json",
+                fileMatch: [""],
+                schema: ActionSchema,
+              },
+            ],
+          });
+        }}
+      />
+      <Button
+        onClick={() => dispatch(jsonAction)}
+        //disabled={!validate(jsonAction)}
+      >
+        Run action
+      </Button>
+    </>
   );
 };
 
