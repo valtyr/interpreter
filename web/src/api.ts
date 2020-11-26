e981848d532ef4c70b45f0a197aa324bc84b737a
Valtýr Örn Kjartansson <valtyr@gmail.com>
2020-11-26 01:56:59 +0000

    Update api paths

--- web/src/api.ts
+++ web/src/api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { store } from "./store";
 import { ActionTypes } from "./store/actions";
 
-const CARD_GROUPS = "http://localhost:8080/api/cardgroups";
-const ADD_CARD_GROUP = "http://localhost:8080/api/addcardgroups";
+const CARD_GROUPS = "http://finndu.vin:8888/api/cardgroups";
+const ADD_CARD_GROUP = "http://finndu.vin:8888/api/addcardgroups";
 
 export const fetchCardGroups = async () => {
   store.dispatch({ type: ActionTypes.FetchCardGroups });
--- web/src/ws.ts
+++ web/src/ws.ts
@@ -4,12 +4,12 @@ import SockJS from "sockjs-client";
 export const connect = (): Promise<CompatClient> =>
   new Promise((resolve, reject) => {
     const stompClient = Stomp.over(() => {
-      return new SockJS("http://localhost:8080/ws", undefined);
+      return new SockJS("http://finndu.vin:8888/ws", undefined);
     });
     stompClient.reconnectDelay = 5000;
     stompClient.onStompError = (f) => {
-      console.warn('error', f);
-    }
+      console.warn("error", f);
+    };
 
     try {
       stompClient.connect({}, (frame: string) => {
